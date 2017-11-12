43983ea8eae096caed2ac3455daccf7224a23d32
jsharief <jalaludeen.h@gmail.com>
2017-11-11 20:55:32 -0500

    added the spring strucutre:wq

---
+++ spring5learning/src/main/java/com/unicorn/springframework/spring5learning/Spring5learningApplication.java
@@ -0,0 +1,12 @@
+package com.unicorn.springframework.spring5learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5learningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5learningApplication.class, args);
+	}
+}
